f3868930cb7876a2a79e3e222fc5e64601f5628d
mspr <spriet.maxime.eisti@gmail.com>
2015-10-05 21:58:31 +0200

    Add an edition mode in scene and begin to implement events.

--- guitar_trainer/src/model/fretboardscene.cpp
+++ guitar_trainer/src/model/fretboardscene.cpp
@@ -1,10 +1,13 @@
 #include "fretboardscene.h"
 #include "fretboardxmlhandler.h"
 
+#include <QGraphicsSceneMouseEvent>
+
 using namespace Model;
 
 FretboardScene::FretboardScene(const QRectF& sceneRect, QObject* parent)
 	: QGraphicsScene(sceneRect, parent)
+	, m_editionMode(FRET_EDITION)
 {
 }
 
@@ -12,4 +15,19 @@ void FretboardScene::init(const QString& fileName)
 {
 	FretboardXmlHandler fretboardXmlHandler;
 	const bool result = fretboardXmlHandler.handle(fileName);
+}
+
+void FretboardScene::mousePressEvent(QGraphicsSceneMouseEvent* event)
+{
+	QGraphicsScene::mousePressEvent(event);
+
+	if (event->button() == Qt::RightButton)
+	{
+		m_editionMode = (EditionMode)(((int)m_editionMode + 1) / 2);
+	}
+}
+
+void FretboardScene::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
+{
+	QGraphicsScene::mouseMoveEvent(event);
 }
--- guitar_trainer/src/model/fretboardscene.h
+++ guitar_trainer/src/model/fretboardscene.h
@@ -7,10 +7,23 @@ namespace Model
 {
 	class FretboardScene : public QGraphicsScene
 	{
+		enum EditionMode
+		{
+			FRET_EDITION,
+			STRING_EDITION
+		};
+
 		public:
 			FretboardScene(const QRectF& sceneRect, QObject* parent);
 
 			void init(const QString& fileName);
+
+		protected:
+			virtual void mousePressEvent(QGraphicsSceneMouseEvent* event);
+			virtual void mouseMoveEvent(QGraphicsSceneMouseEvent* event);
+
+		private:
+			EditionMode m_editionMode;
 	};
 
 } // Model
